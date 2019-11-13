0c9e85a541be4f5f890ef61346efb58248b37f1c
mo2menelzeiny <mo2menelzeiny@gmail.com>
2019-11-13 14:06:53 +0200

    chore: add palindrome commments

--- src/palindrom.rb
+++ src/palindrom.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
+
+# Palindrome Permutation: Given a string, write a function to check if it is a permutation of
+# a palindrome. A palindrome is a word or phrase that is the same forwards and backwards. A
+# permutation is a rearrangement of letters. The palindrome does not need to be limited to just
+# dictionary words.
+
 # @param [String] value
 def is_palindrome(value)
   counts = {}
