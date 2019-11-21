073286283f5dcce8cef36bd0cbda727a5da2835d
mo2menelzeiny <mo2menelzeiny@gmail.com>
2019-11-21 20:45:32 +0200

    feat: delete at

--- src/linked_list/linked_list.rb
+++ src/linked_list/linked_list.rb
@@ -42,6 +42,22 @@ class LinkedList
       current.next.next.prev = current.next
     end
   end
+
+  # @param[Object] target
+  # @return [LinkedNode]
+  def delete_at(target)
+    current = @head
+    current = current.next until current.next.nil? || current.value == target
+    return current if current == @tail
+
+    if current.next == @tail
+      current.next = nil
+      @tail = current
+    else
+      current.next = current.next.next
+      current.next.prev = current
+    end
+  end
 end
 
 root_node = LinkedNode.new(0)
@@ -53,3 +69,4 @@ list.append_to_tail(4)
 list.insert_at(2, 3)
 list.insert_at(22, 5)
 list.remove_from_tail
+list.delete_at 4
