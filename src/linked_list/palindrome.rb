cfe3175505753f8e6e90417471e9929bd8426664
mo2menelzeiny <mo2menelzeiny@gmail.com>
2019-12-02 20:57:50 +0200

    fix: linked list palindrome recursive approach

--- src/linked_list/palindrome.rb
+++ src/linked_list/palindrome.rb
@@ -1,25 +1,17 @@
 require_relative './linked_node'
 # Palindrome: Implement a function to check if a linked list is a palindrome.
 
-# @param[LinkedNode] list
-def is_palindrome(list)
-  length = 0
-  char_count = {}
-  curr = list
-  until curr.nil?
-    if char_count[curr.value].nil?
-      char_count[curr.value] = 1
-    else
-      char_count[curr.value] += 1
-    end
-    length += 1
-    curr = curr.next
-  end
-  odd = 0
-  char_count.each_value do |val|
-    odd += 1 if val.odd?
-  end
-  odd < 2
+# @param[LinkedNode] node
+def is_palindrome(node, length)
+  return [node, true] if node.nil? || length <= 0
+
+  return [node.next, true] if length == 1
+
+  rnode, status = is_palindrome(node.next, length - 2)
+
+  return [rnode, status] if !status || rnode.nil?
+
+  [rnode.next, (node.value == rnode.value)]
 end
 
 head_a = list_a = LinkedNode.new('b')
@@ -27,18 +19,18 @@ list_a = list_a.next = LinkedNode.new('a')
 list_a = list_a.next = LinkedNode.new('a')
 list_a.next = LinkedNode.new('b')
 
-puts is_palindrome(head_a)
+puts is_palindrome(head_a, 4)
 
 head_b = list_b = LinkedNode.new('b')
 list_b = list_b.next = LinkedNode.new('b')
 list_b = list_b.next = LinkedNode.new('a')
 list_b.next = LinkedNode.new('b')
 
-puts is_palindrome(head_b)
+puts is_palindrome(head_b, 4)
 
 
 head_c = list_c = LinkedNode.new('1')
 list_c = list_c.next = LinkedNode.new('0')
 list_c.next = LinkedNode.new('0')
 
-puts is_palindrome(head_c)
+puts is_palindrome(head_c, 3)
