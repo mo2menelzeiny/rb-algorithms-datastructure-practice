191b2b623bde56241e96c1bae95cb26b3db5b69e
mo2menelzeiny <mo2menelzeiny@gmail.com>
2019-12-24 09:22:54 +0200

    feat: graph implementation

---
+++ trees_graphs/graph.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+# @!attribute value
+#   @return [Object]
+# @!attribute children
+#   @return [Array<GraphNode>]
+class GraphNode
+  attr_accessor :value, :children
+
+  def initialize(value = nil, children = [])
+    @value = value
+    @children = children
+  end
+end
+
+# @!attribute nodes
+#   @return [Array<GraphNode>]
+class Graph
+  attr_accessor :nodes
+
+  def initialize(nodes = [])
+    @nodes = nodes
+  end
+end
+
+l3 = [GraphNode.new('F')]
+l2 = [GraphNode.new('C'), GraphNode.new('D', l3), GraphNode.new('E')]
+l1 = [GraphNode.new('B', l2)]
+l0 = [GraphNode.new('A', l1)]
+
+graph = Graph.new(l0)
+
