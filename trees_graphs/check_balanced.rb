b38cea11716dcbe4a2034a1eae9bf66c460b1688
mo2menelzeiny <mo2menelzeiny@gmail.com>
2019-12-26 10:48:20 +0200

    feat(4.4): check balanced

---
+++ trees_graphs/check_balanced.rb
@@ -0,0 +1,54 @@
+# Check Balanced: Implement a function to check if a binary tree is balanced. For the purposes of
+# this question, a balanced tree is defined to be a tree such that the heights of the two subtrees of any
+# node never differ by more than one.
+
+# @param [Hash<Array>] tree
+# @param [String] root
+def check_nodes(tree, root, height)
+  return height if tree[root].empty?
+
+  check_nodes(tree, tree[root][0], height + 1)
+end
+
+# @param [Hash<Array>] tree
+# @param [String] root
+def check_balanced(tree, root)
+  left = check_nodes(tree, tree[root][0], 1)
+  right = check_nodes(tree, tree[root][1], 1)
+  result = (left - right).abs
+  result.zero? || result == 1
+end
+
+tree = {
+    'A' => ['B', 'C'],
+    'B' => ['D', 'E'],
+    'C' => ['F', 'G'],
+    'D' => ['H'],
+    'E' => [],
+    'F' => [],
+    'G' => [],
+    'H' => ['F']
+}
+
+puts check_balanced(tree, 'A')
+
+tree = {
+    'A' => ['B', 'C'],
+    'B' => ['D', 'E'],
+    'C' => ['F', 'G'],
+    'D' => ['H'],
+    'E' => [],
+    'F' => [],
+    'G' => [],
+    'H' => []
+}
+
+puts check_balanced(tree, 'A')
+
+tree = {
+    'A' => ['B', 'C'],
+    'B' => [],
+    'C' => [],
+}
+
+puts check_balanced(tree, 'A')
